2d51eb98e0393e7a5a17c01fe4e62ffc96890745
lukaszgolojuch <lukasz.golojuch@gmail.com>
2022-05-23 23:35:43 +0200

    Initial Commit

---
+++ Gdansk Tour/Gdansk_TourApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gdansk_TourApp.swift
+//  Gdansk Tour
+//
+//  Created by Łukasz Gołojuch on 5/23/22.
+//
+
+import SwiftUI
+
+@main
+struct Gdansk_TourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
